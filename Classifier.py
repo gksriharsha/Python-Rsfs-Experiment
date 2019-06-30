e6dc98881e5af29928749f3ebe20c84b90202ba2
Harsha <gksriharsha@gmail.com>
2019-06-30 14:53:58 +0530

    Final git ignore commit

--- Classifier.py
+++ Classifier.py
@@ -1,6 +1,7 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
+
 def Classifier_I(Feature_train, Feature_test, label_train, Parameters):
     if (Parameters['Classifier'] == 'KNN'):
         K = 3
