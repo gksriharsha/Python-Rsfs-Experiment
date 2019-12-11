684cd2f64c227470e558eb72e9a6323e3fcb9b21
gksriharsha <gksriharsha@users.noreply.github.com>
2019-12-11 22:15:59 +0530

    Additions for Cloud

--- Dataset_Loader.py
+++ Dataset_Loader.py
@@ -1,10 +1,34 @@
 import numpy
 from sklearn.model_selection import train_test_split
-def getdataset():
-    Data = numpy.loadtxt(open(str('Isolet.csv'), "rb"), delimiter=",", skiprows=1);
-    labels = Data[:, -1]
-    Data = Data[:,:-1]
-    train, test, train_labels, test_labels = train_test_split(
-                Data, labels, test_size=0.33, random_state=42, stratify=labels)
-    print((train_labels == train[:,-1]))
+from sklearn import preprocessing
+def getdataset(i):
+    if(i == 1):
+        Data = numpy.loadtxt(open(str('Isolet.csv'), "rb"), delimiter=",", skiprows=1);
+        labels = Data[:, -1]
+        Data = Data[:,:-1]
+        train, test, train_labels, test_labels = train_test_split(
+                    Data, labels, test_size=0.33, random_state=42, stratify=labels)
+        print((train_labels == train[:,-1]))
+    if(i == 2):
+        Data = numpy.loadtxt(open(str('AP_Breast_Lung.csv'), "rb"), delimiter=",", skiprows=1);
+        pre_labels = Data[:, -1]
+        le = preprocessing.LabelEncoder()
+        labels = le.fit_transform(pre_labels)
+        Data = Data[:,:-1]
+        train, test, train_labels, test_labels = train_test_split(
+                    Data, labels, test_size=0.33, random_state=42, stratify=labels)
+        print((train_labels == train[:,-1]))
+    if(i == 3):
+        Data = numpy.loadtxt(open(str('Bioresponse.csv'), "rb"), delimiter=",", skiprows=1);
+        labels = Data[:, -1]
+        Data = Data[:,:-1]
+        train, test, train_labels, test_labels = train_test_split(
+                    Data, labels, test_size=0.33, random_state=42, stratify=labels)
+        print((train_labels == train[:,-1]))
+    if(i == 4):
+        Data = numpy.loadtxt(open(str('gas-drift.csv'), "rb"), delimiter=",", skiprows=1);
+        labels = Data[:, -1]
+        Data = Data[:,:-1]
+        train, test, train_labels, test_labels = train_test_split(
+                    Data, labels, test_size=0.33, random_state=42, stratify=labels)
     return train, test,train_labels,test_labels
--- Exec.py
+++ Exec.py
@@ -4,85 +4,93 @@ from sklearn.model_selection import train_test_split
 import random
 import csv
 from Dataset_Loader import train,test,train_labels,test_labels,getdataset
+import ttictoc
+for dataset_number in range(4):
+    train,test,train_labels,test_labels = getdataset(dataset_number)
+    Feature_train = train
+    Feature_test = test
+    label_train = train_labels
+    label_test = test_labels
 
-train,test,train_labels,test_labels = getdataset()
-Feature_train = train
-Feature_test = test
-label_train = train_labels
-label_test = test_labels
-
-Parameters = {
-     'Top2' : 0,
-    'RSFS' : {
-        'K' : 3,
-        'Dummy feats' : 100,
-        'delta' : 0.05,
-        'maxiters' : 300000,
-        'fn' : 'sqrt',
-        'cutoff' : 0.99,
-        'stored' : 0,
-        'top' : 2,
-        'Threshold' : 1000,
-    },
-    'Classifier':'KNN'
-}
-
-States = {
-    'RSFS':{
-        'stored': [0,1],
-        'K' : 3,
-        'Top2' : [0,1],
-        'Dummy feats' : list(numpy.arange(100,numpy.size(train,1),100)),
-        'fn' : ['sqrt','10log'],
-        'top' : list(range(1,7)),
-        'Threshold' : list(numpy.arange(500,2400,100)),
-	    'cutoff': [0.95,0.96,0.97,0.98,0.99,0.997]
+    Parameters = {
+        'Top2' : 0,
+        'RSFS' : {
+            'K' : 3,
+            'Dummy feats' : 100,
+            'delta' : 0.05,
+            'maxiters' : 300000,
+            'fn' : 'sqrt',
+            'cutoff' : 0.99,
+            'stored' : 0,
+            'top' : 2,
+            'Threshold' : 1000,
+        },
+        'Classifier':'KNN'
     }
-}
 
-Combinations = numpy.array([])
-limit = [len(States['RSFS']['Dummy feats']),len(States['RSFS']['Threshold']) , len(States['RSFS']['fn']),
-                       len(States['RSFS']['cutoff'])]
-n= numpy.prod(limit)
-for i in list(numpy.arange(1, len(limit) + 1)):
-    li = [];
-    for ii in list(numpy.arange(1,limit[i-1]+1)):
-        li = list(numpy.append(li,numpy.matlib.repmat(ii , 1, int(n/numpy.prod(limit[0:i])))))
-    addend = numpy.matlib.repmat(li,1,int(n/numpy.size(li)))
-    Combinations = numpy.vstack((Combinations,addend)) if Combinations.size else addend
+    States = {
+        'RSFS':{
+            'stored': [0,1],
+            'K' : 3,
+            'Top2' : [0,1],
+            'Dummy feats' : list(numpy.arange(100,numpy.size(train,1),100)),
+            'fn' : ['sqrt','10log'],
+            'top' : list(range(1,7)),
+            'Threshold' : list(numpy.arange(500,2400,100)),
+            'cutoff': [0.95,0.96,0.97,0.98,0.99,0.997]
+        }
+    }
 
-Combinations = numpy.transpose(Combinations)
-#Combinations = list(Combinations)
-print(Combinations)
+    Combinations = numpy.array([])
+    limit = [len(States['RSFS']['Dummy feats']),len(States['RSFS']['Threshold']) , len(States['RSFS']['fn']),
+                        len(States['RSFS']['cutoff'])]
+    n= numpy.prod(limit)
+    for i in list(numpy.arange(1, len(limit) + 1)):
+        li = []
+        for ii in list(numpy.arange(1,limit[i-1]+1)):
+            li = list(numpy.append(li,numpy.matlib.repmat(ii , 1, int(n/numpy.prod(limit[0:i])))))
+        addend = numpy.matlib.repmat(li,1,int(n/numpy.size(li)))
+        Combinations = numpy.vstack((Combinations,addend)) if Combinations.size else addend
 
-j = 0
-heading = ['S.No','Threshold','K','fn','Dummy feats','Orig_Acc','RSFS_Acc','Orig_time','RSFS_time','Total Features','RSFS features','Iteration']
-with open('Performance_RSFS.csv', 'w', newline='') as myfile:
-    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    wr.writerow(heading)
-    wr.writerow("\n")
-while True:
-    if(j == numpy.size(Combinations,axis=0)):
-        break
-    else:
-        random.seed(0)
-        numpy.random.seed(0)
+    Combinations = numpy.transpose(Combinations)
+    #Combinations = list(Combinations)
+    print(Combinations)
+    #####################Helps in Multi core environments#############################
+    from dask.distributed import Client, LocalCluster
+    cluster = LocalCluster()
+    client = Client(cluster)
+    import joblib
+    ##################################################
+    j = 0
+    heading = ['S.No','Threshold','K','fn','Dummy feats','Orig_Acc','RSFS_Acc','Orig_time','RSFS_time','Total Features','RSFS features','Iteration']
+    with open('Performance_RSFS.csv', 'w', newline='') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(heading)
+        wr.writerow("\n")
+    while True:
+        if(j == numpy.size(Combinations,axis=0)):
+            break
+        else:
+            random.seed(0)
+            numpy.random.seed(0)
 
 
-        Parameters['RSFS']['cutoff'] = States['RSFS']['cutoff'][int(Combinations[j, 4])-1]
-        Parameters['RSFS']['Threshold'] = States['RSFS']['Threshold'][int(Combinations[j, 2])-1]
-        Parameters['RSFS']['fn'] = States['RSFS']['fn'][int(Combinations[j, 3])-1]
-        Parameters['RSFS']['K'] = States['RSFS']['K'][int(Combinations[j, 1])-1]
-        Parameters['RSFS']['Dummy feats'] = States['RSFS']['Dummy feats'][int(Combinations[j, 0])-1]
-        print(Parameters)
-
-
-        Result = RSFS.Run_Class(Feature_train,Feature_test,label_train,label_test,Parameters)
-        line = [j,Parameters['RSFS']['cutoff'],Parameters['RSFS']['Threshold'],Parameters['RSFS']['K'],Parameters['RSFS']['fn'],Parameters['RSFS']['Dummy feats'],Result['Orig']['Accuracy'],Result['RSFS']['Accuracy'],
-                Result['Orig']['Time'],Result['RSFS']['Time'],Result['Orig']['Features'],numpy.size(Result['RSFS']['Feats']),Result['RSFS']['Iteration']]
-
-        with open('Performance_RSFS.csv', 'a', newline='') as myfile:
-            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-            wr.writerow(line)
-        print('Combination  :',j)
-        j = j+1
+            Parameters['RSFS']['cutoff'] = States['RSFS']['cutoff'][int(Combinations[j, 4])-1]
+            Parameters['RSFS']['Threshold'] = States['RSFS']['Threshold'][int(Combinations[j, 2])-1]
+            Parameters['RSFS']['fn'] = States['RSFS']['fn'][int(Combinations[j, 3])-1]
+            Parameters['RSFS']['K'] = States['RSFS']['K'][int(Combinations[j, 1])-1]
+            Parameters['RSFS']['Dummy feats'] = States['RSFS']['Dummy feats'][int(Combinations[j, 0])-1]
+            print(Parameters)
+            t = ttictoc.TicToc
+            t.tic()
+            with joblib.parallel_backend('dask'):
+                Result = RSFS.Run_Class(Feature_train,Feature_test,label_train,label_test,Parameters)
+            line = [j,len(train_labels)+len(test_labels),Feature_train.size[1],len(numpy.unique(train_labels)),Parameters['RSFS']['cutoff'],Parameters['RSFS']['Threshold'],Parameters['RSFS']['K'],Parameters['RSFS']['fn'],Parameters['RSFS']['Dummy feats'],Result['Orig']['Accuracy'],Result['RSFS']['Accuracy'],
+                    Result['Orig']['Time'],Result['RSFS']['Time'],Result['Orig']['Features'],numpy.size(Result['RSFS']['Feats']),Result['RSFS']['Iteration']]
+            t.toc()
+            print(t.elapsed+ " seconds")
+            with open('Performance_RSFS.csv', 'a', newline='') as myfile:
+                wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+                wr.writerow(line)
+            print('Combination  :',j)
+            j = j+1
